47e66eb519b41c26927a9986201bbe8eaf18baee
Modo Network <helpdesk@modonetwork.com>
2017-03-21 16:56:15 +0100

    Add files via upload

--- build/mm.bundle.js
+++ build/mm.bundle.js
@@ -9556,10 +9556,13 @@ angular.module('mm.core.login', [])
             });
         }]
     })
-    .state('mm_login.site', {
-        url: '/site',
+    .state('mm_login.site', { 
+        url: '/site', 
         templateUrl: 'core/components/login/templates/site.html',
-        controller: 'mmLoginSiteCtrl'
+        controller: 'mmLoginSiteCtrl',
+        onEnter: function($state) {
+            $state.go('mm_login.credentials', {siteurl: 'http://test.modonetwork.com'});
+        }
     })
     .state('mm_login.credentials', {
         url: '/cred',
